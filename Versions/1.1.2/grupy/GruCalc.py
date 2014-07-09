2b3e8975fda837aa2adf180fac71a2a7de396e3e
Alex Miller <asmiller1989@gmail.com>
2014-07-09 18:41:39 -0500

    1.1.2 should be fixed..

--- Versions/1.1.2/grupy/GruCalc.py
+++ Versions/1.1.2/grupy/GruCalc.py
@@ -1,213 +1,196 @@
-##diagonalize
+# #diagonalize
 import numpy as np
 from qe.ReadDynMat import *
 import cmath
 
 
-def Get_Eval_Evec_dDdV  ( Darray , BZ_path ):
-	
-	#Deq = []   ## equilibrium dyn matrices
-	Dn = []		## non-equilibrium dyn matrices
-	dD = []   ## what will be returned (dD/dV)
-	EVAL = []	## eigenvalues (omega^2) 
-	EVEC = []	## eigenvectors that diagonalize eq matrices
+def Get_Eval_Evec_dDdV(Darray, BZ_path):
+    #Deq = []   ## equilibrium dyn matrices
+    Dn = []  ## non-equilibrium dyn matrices
+    dD = []  ## what will be returned (dD/dV)
+    EVAL = []  ## eigenvalues (omega^2)
+    EVEC = []  ## eigenvectors that diagonalize eq matrices
 
-	
+    for i in range(len(Darray)):  # 3
 
-	
-	for i in range( len (Darray) ):  # 3
-		
-		if i != 0:  ## i.e. not an equilibrium matrix
-		
-			Dn.append([])
+        if i != 0:  ## i.e. not an equilibrium matrix
 
-		
-		for j in xrange( len( Darray[i]) ):  # number of q points
-		
-			
-					
-			if i == 0: ## again, just looking at equilibrium matrices
-				
-				EVAL.append( [] )
-				EVEC.append( [] )
-			
-				eval, evec = np.linalg.eigh( Darray[i][j] )
+            Dn.append([])
 
-				#Deq.append ( Darray[0][j] )
-				
-				for k in xrange( len(eval) ):  # 3N 
-					# TESTING: should be 3N now
-					EVAL[j].append(eval[k])
-					EVEC[j].append(evec[k])
-				
-					
-			
-			else: ## i.e. NON-equilibrium matrices
-				Dn[i-1].append(Darray[i][j])  ## this is a full dyn matrix
-				
-	
-	# Dn array should have 2 columns of elements (for + and - volume calculations)
-	#	, with each element being a 3Nx3N matrix of eigenvalues at that q-point
-	# Deq array should have three dimensions	
-	
-	
-	for i in xrange(len(Darray[0])):  # q points
-		
-		dD.append([])
-		
-		for j in xrange( len(Darray[0][i]) ):  # 3N
-			
-			dD[i].append([])
-			
-			for k in xrange( len(Darray[0][i][j]) ):  # 3N
-			
-				dD[i][j].append( (Dn[0][i][j][k] - Dn[1][i][j][k]) )
-				# Dn[0] is D+ , Dn[1] is D-
-	
-	
-	return EVAL, EVEC, dD
+        for j in xrange(len(Darray[i])):  # number of q points
+
+            if i == 0:  ## again, just looking at equilibrium matrices
+
+                EVAL.append([])
+                EVEC.append([])
+
+                eval, evec = np.linalg.eigh(Darray[i][j])
+
+                #Deq.append ( Darray[0][j] )
+
+                for k in xrange(len(eval)):  # 3N
+                    # TESTING: should be 3N now
+                    EVAL[j].append(eval[k])
+                    EVEC[j].append(evec[k])
+
+
+
+            else:  ## i.e. NON-equilibrium matrices
+                Dn[i - 1].append(Darray[i][j])  ## this is a full dyn matrix
+
+
+    # Dn array should have 2 columns of elements (for + and - volume calculations)
+    #	, with each element being a 3Nx3N matrix of eigenvalues at that q-point
+    # Deq array should have three dimensions
+
+    if Dn:
+        for i in xrange(len(Darray[0])):  # q points
+            dD.append([])
+            for j in xrange(len(Darray[0][i])):  # 3N
+                dD[i].append([])
+
+                for k in xrange(len(Darray[0][i][j])):  # 3N
+
+                    # Dn[0] is D+ , Dn[1] is D-
+                    dD[i][j].append((Dn[0][i][j][k] - Dn[1][i][j][k]))
+
+
+    return EVAL, EVEC, dD
 
 
 ## Calculate Gruneisen parameter
 
-def GruCalc ( Gin, q, Darray):
+def GruCalc(Gin, q, Darray):
+    V = Gin.V
+    nat = Gin.nat
+    amass = Gin.m
+    BZ_path = Gin.BZ_path
 
-	
-	V = Gin.V
-	nat = Gin.nat
-	amass = Gin.m
-	BZ_path = Gin.BZ_path
 
-	eval_eq, evec_eq, dD = Get_Eval_Evec_dDdV ( Darray, BZ_path)  # get equilibrium eigenvalues
-											   # and eigenvectors as well as the
-												   # dD matrix
-	dD = np.asarray(dD)
-	evec_eq = np.asarray(evec_eq)
-			
-	
-	gru = [] #the Gruneisen parameter
-	gru_i = [] #the mode index of said Gruneisen parameter
-	
-	gru.append([]) # for the q point column
-	gru_i.append([])
-	
-	for i in xrange( len(dD) ):  ## should be equal to the number of q points
-		
-		gru[0].append(BZ_path[i])
-		gru_i[0].append(BZ_path[i])
-	
-		
-		# SORTING of eigenvalues: must correspond to eigenvectors whose inner product
-		# with that of previous q point is 1
-		#  e.g. < eig1(q=0) | eig1(q=1) >   = 1
-		#       < eig1(q=0) | eig2(q=1) >   = 0
-		
-		index = []
-		sort = []
-		if i > 0:
-	
-			for x in xrange( len(evec_eq[i]) ):
+    # get equilibrium eigenvalues, equilibrium eigenvectors, and dD (nonequilibrium differences) matrix
+    eval_eq, evec_eq, dD = Get_Eval_Evec_dDdV(Darray, BZ_path)  # get equilibrium eigenvalues
 
-				temp = [] 
-				proj = []
-				rel = []
-	
-				for y in xrange( len(evec_eq[i-1]) ):
-						
-					# Project, i.e. take the inner product of the two vectors
-					p = np.vdot( evec_eq[i][:,x].transpose() , evec_eq[i-1][:,y] )
-					proj.append( abs(p) )
-					rel.append( [x,y])
-					
-				for z in xrange( len(proj) ):
-						
-					if proj[z] == max(proj):
-						index.append([proj[z],rel[z][0], rel[z][1] ])
-						
+    dD = np.asarray(dD)
+    evec_eq = np.asarray(evec_eq)
 
-		
-		if i == 0:  # the first q point
-		
-			for j in xrange(  len(dD[i])  ):  ## 3N
+    gru = []  # the Gruneisen parameter
+    gru_i = []  # the mode index of said Gruneisen parameter
 
-				gru.append([])
-				gru_i.append([])
+    gru.append([])  # for the q point column
+    gru_i.append([])
 
-				evec_0 = np.asarray( evec_eq[i] )  # 3N
-				bra = evec_0[:,j]
-				ket = evec_0[:,j].transpose()
-				
-				d_eval = abs( np.vdot( bra, (np.dot(dD[i] , ket) ) ) )
-				
-				
-##########################################
-##   HERE WE CALCULATE GRUNEISEN PARAMETER
-###################################################	
-				
-			# throw away very low values of d_omega^2 because these won't contribute to 
-			# thermal conductivity and they will result in large, misleading Gruneisen parameters
-			
-				if abs(d_eval) < 1e-8:  # AND abs(eval_eq[i][j]) < ~ 0.3 THz
-					
-					g = 0.0
-			
-				else:
-					g = - ( d_eval/(2*eval_eq[i][j]) )* ( V[0]/(V[1]-V[2]))
+    for i in xrange(len(eval_eq)):  # should be equal to the number of q points
 
-				gru[j+1].append( float(g) )
-				gru_i[j+1].append( j )
+        gru[0].append(BZ_path[i])
+        gru_i[0].append(BZ_path[i])
 
-			# Note: V elements MUST correspond to the correct calculations
-			# V[0] = Veq ; V[1] = V+ ; V[2] = V-
-			# I may want to change this later...
-		
-	
-		else: # all subsequent q points: need to use the index variable for sorting
-	
-			for j in xrange( len(dD[i]) ): # 3N
-				
-#
-# We're going through the 3N items in the index
-#				
-				for x in xrange( len(index) ):
-#
-# Check to see if the second number (i.e. the ket of the sorting projection)
-# in the index is the mode we want to write a value to
-#
 
-					if index[x][1] == j: 
-		
-						evec_q = np.asarray( evec_eq[i])
-						
-						
-						bra = evec_q[:,x]
-						ket = evec_q[:,x].transpose()
-						
-						d_eval = abs( np.dot( bra, (np.dot(dD[i] , ket) ) ) )
-					
-					
-					## REJECT values where both eigenvalues are very small.
-					## These are prone to error and have little physical meaning
-					## NOTE: 2e-9 is somewhat arbitrary: it was the lowest value
-					#		I could assign where all of the Gru values stayed near
-					#		the peaks at gamma in PbTe
-					
-					#d_eval is the eigenvalue (omega^2) for the non-equilibrium V
-					# eval_eq is the eigenvalue (omega^2) for the equilibrium V
-						
-						if abs(d_eval) < 2e-9 and abs(eval_eq[i][j]) < 2e-9:
-							g = 0.0
-							
-			
-						else:
-							g = - ( d_eval/(2*eval_eq[i][j]) )* ( V[0]/(V[1]-V[2]))
-	
-						gru[j+1].append( float(g) )
-						gru_i[j+1].append( j )
-						
-				
-		
+        # SORTING of eigenvalues: must correspond to eigenvectors whose inner product
+        # with that of previous q point is 1
+        #  e.g. < eig1(q=0) | eig1(q=1) >   = 1
+        #       < eig1(q=0) | eig2(q=1) >   = 0
 
-	return q, gru, gru_i		
-	
-		
+        index = []
+        sort = []
+        if i > 0:
+
+            for x in xrange(len(evec_eq[i])):
+
+                temp = []
+                proj = []
+                rel = []
+
+                for y in xrange(len(evec_eq[i - 1])):
+                    # Project, i.e. take the inner product of the two vectors
+                    p = np.vdot(evec_eq[i][:, x].transpose(), evec_eq[i - 1][:, y])
+                    proj.append(abs(p))
+                    rel.append([x, y])
+
+                for z in xrange(len(proj)):
+
+                    if proj[z] == max(proj):
+                        index.append([proj[z], rel[z][0], rel[z][1]])
+
+        if i == 0:  # the first q point
+
+            for j in xrange(len(evec_eq[i])):  # 3N; previously used dD instead of evec_eq but the dimensions should be the same
+
+                gru.append([])
+                gru_i.append([])
+
+                evec_0 = np.asarray(evec_eq[i])  # 3N
+                bra = evec_0[:, j]
+                ket = evec_0[:, j].transpose()
+
+                if dD:
+                    d_eval = abs(np.vdot(bra, (np.dot(dD[i], ket) )))
+                else:
+                    d_eval = 0.0
+
+                ##########################################
+                ##   HERE WE CALCULATE GRUNEISEN PARAMETER
+                ###################################################
+
+                # throw away very low values of d_omega^2 because these won't contribute to
+                # thermal conductivity and they will result in large, misleading Gruneisen parameters
+
+                if abs(d_eval) < 1e-8:  # AND abs(eval_eq[i][j]) < ~ 0.3 THz
+                    g = 0.0
+                else:
+                    if len(V) > 1:
+                        g = - ( d_eval / (2 * eval_eq[i][j]) ) * ( V[0] / (V[1] - V[2]))
+
+                gru[j + 1].append(float(g))
+                gru_i[j + 1].append(j)
+
+            # Note: V elements MUST correspond to the correct calculations
+            # V[0] = Veq ; V[1] = V+ ; V[2] = V-
+            # I may want to change this later...
+
+
+        else:  # all subsequent q points: need to use the index variable for sorting
+
+            for j in xrange(len(eval_eq[i])):  # 3N (again using eval_eq instead of dD)
+
+                #
+                # We're going through the 3N items in the index
+                #
+                for x in xrange(len(index)):
+                    #
+                    # Check to see if the second number (i.e. the ket of the sorting projection)
+                    # in the index is the mode we want to write a value to
+                    #
+
+                    if index[x][1] == j:
+
+                        evec_q = np.asarray(evec_eq[i])
+
+                        bra = evec_q[:, x]
+                        ket = evec_q[:, x].transpose()
+
+                        if dD:
+                            d_eval = abs(np.dot(bra, (np.dot(dD[i], ket) )))
+                        else:
+                            d_eval = 0.0
+
+                        ## REJECT values where both eigenvalues are very small.
+                        ## These are prone to error and have little physical meaning
+                        ## NOTE: 2e-9 is somewhat arbitrary: it was the lowest value
+                        #		I could assign where all of the Gru values stayed near
+                        #		the peaks at gamma in PbTe
+
+                        #d_eval is the eigenvalue (omega^2) for the non-equilibrium V
+                        # eval_eq is the eigenvalue (omega^2) for the equilibrium V
+
+                        if abs(d_eval) < 2e-9 and abs(eval_eq[i][j]) < 2e-9:
+                            g = 0.0
+
+                        else:
+                            if len(V) > 1:
+                                g = - ( d_eval / (2 * eval_eq[i][j]) ) * ( V[0] / (V[1] - V[2]))
+
+                        gru[j + 1].append(float(g))
+                        gru_i[j + 1].append(j)
+
+    return q, gru, gru_i
+
--- Versions/testing/grupy.py
+++ Versions/testing/grupy.py
@@ -2,11 +2,9 @@
 import os
 import sys
 
-## append path to grupy modules
+# # append path to grupy modules
 cwd = os.path.dirname(os.path.abspath(__file__))
-sys.path.append( os.path.join( cwd, "..") )
-
-
+sys.path.append(os.path.join(cwd, ".."))
 
 from grupy import *
 from grupy.qe import *
@@ -15,6 +13,8 @@ from optparse import OptionParser
 import subprocess as sub
 
 
+# Welcome to grupy!
+print "\n!-----------   grupy! version 1.1.2   -----------!\n"
 
 
 # should eventually write some flags for bad data input
@@ -24,26 +24,22 @@ import subprocess as sub
 dir, in_path = GetInput()
 
 
-
-
 ## Parse options to graph data and to make/run q2r.in and matdyn.in scripts 
 ###########################################################################
 
 parser = OptionParser()
-parser.set_defaults( 
-					 make_scripts = False,
-					 run_scripts = False,
-					 bands = False,
-                     single = False)
-
+parser.set_defaults(
+    make_scripts=False,
+    run_scripts=False,
+    bands=False,
+    single=False)
 
 parser.add_option("--make", dest="make_scripts", action="store_true")
 parser.add_option("--run", dest="run_scripts", action="store_true")
 parser.add_option("--bands", dest="bands", action="store_true")
-parser.add_option("-s", dest="single", action="store")   #plot a phonon bands structure without needing 3 calculations
+parser.add_option("-s", dest="single", action="store")  #plot a phonon bands structure without needing 3 calculations
 
-
-(options, args) = parser.parse_args() 
+(options, args) = parser.parse_args()
 
 
 
@@ -51,56 +47,51 @@ parser.add_option("-s", dest="single", action="store")   #plot a phonon bands st
 
 #### grupy_in object
 ####################
-Gin = grupy_in(dir, in_path, None , None, None, None, None, None, None, None, None)
+Gin = grupy_in(dir, in_path, None, None, None, None, None, None, None, None, None)
 
+Gin.prefix = GetPrefix(Gin.dir)
+Gin.path, Gin.hs_points = MakePath(Gin.in_path)
 
-Gin.prefix = GetPrefix( Gin.dir )
-Gin.path, Gin.hs_points = MakePath( Gin.in_path )
-
-
-
-
-(Gin.nat, Gin.V, Gin.m) = GetNat_M_V( Gin.dir, Gin.prefix)
+(Gin.nat, Gin.V, Gin.m) = GetNat_M_V(Gin.dir, Gin.prefix)
 
 if options.make_scripts:
-	MakeMatdyn( Gin.dir, Gin.prefix, Gin.path, Gin.m)
-	MakeQ2r(Gin.prefix, Gin.dir)
-	print "\nq2r.in and matdyn.in files created\n"
-	sys.exit()
+    MakeMatdyn(Gin.dir, Gin.prefix, Gin.path, Gin.m)
+    MakeQ2r(Gin.prefix, Gin.dir)
+    print "\nq2r.in and matdyn.in files created\n"
+    sys.exit()
 
 # this doesn't work - I need to write a shell script
 if options.run_scripts:
-	
-	for i in range(len(Gin.dir) ):
-		
-		q = 'cd %s && q2r.x < q2r.in && cd ..'%(Gin.dir[i])
-		sub.call( q , shell=True)
 
-		m = 'cd %s && matdyn.x < matdyn.in && cd ..'%(Gin.dir[i])
-		sub.call( m , shell=True)
-		
-		print "dynamical matrix files created"
-	sys.exit()
-	
-	
+    for i in range(len(Gin.dir)):
+        q = 'cd %s && q2r.x < q2r.in && cd ..' % (Gin.dir[i])
+        sub.call(q, shell=True)
+
+        m = 'cd %s && matdyn.x < matdyn.in && cd ..' % (Gin.dir[i])
+        sub.call(m, shell=True)
+
+        print "dynamical matrix files created"
+    sys.exit()
+
+
 
 
 
 ## Generate the BZ path and corresponding high symmetry labels
-Gin.BZ_path, BZ_labels = MakeBZPath( Gin.hs_points, Gin.path)
+Gin.BZ_path, BZ_labels = MakeBZPath(Gin.hs_points, Gin.path)
 
 ## Get the data from QE output text files
-q, Darray = ReadDynMat ( Gin.nat, Gin.m, Gin.dir)
+q, Darray = ReadDynMat(Gin.nat, Gin.m, Gin.dir)
 
 ## Format the data to be written to a formatted output file
-Gin.q, gru_data, mode_index = GruCalc ( Gin, q, Darray)
+Gin.q, gru_data, mode_index = GruCalc(Gin, q, Darray)
 
-
-	
 if not options.bands:
-	Gout = grupy_out( Gin.prefix, BZ_labels , gru_data, mode_index, None   )
-	WriteGrupyFile(Gout, Gin, 0)
-	print "\ngrupy.out file written\n"
+
+
+    Gout = grupy_out(Gin.prefix, BZ_labels, gru_data, mode_index, None)
+    WriteGrupyFile(Gout, Gin, 0)
+    print "\ngrupy.out file written\n"
 
 
 
@@ -108,18 +99,19 @@ if not options.bands:
 if options.bands:
 
     if options.single:
-
         # Match a variable in Gin.dir list with the directory supplied with the -s option
-        index = int([i for i in range(len(Gin.dir)) if Gin.dir[i] ==options.single][0])
+        if options.single[0] not in Gin.dir:
+            print "\nCalculation is not in the current directory. Exiting.\n"
+            sys.exit()
+        index = int([i for i in range(len(Gin.dir)) if Gin.dir[i] == options.single][0])
 
         #Recreate lists of single items for Gin variables
-        Gin.dir= [Gin.dir[index]]
+        Gin.dir = [Gin.dir[index]]
         Gin.V = [Gin.V[index]]
 
-    bands = GetBands( Gin )
+    bands = GetBands(Gin)
     group_velocity = GetGroupVelocity(bands)
-    Gout = grupy_out( Gin.prefix, BZ_labels , bands, mode_index, group_velocity   )
-
+    Gout = grupy_out(Gin.prefix, BZ_labels, bands, mode_index, group_velocity)
 
     WriteGrupyFile(Gout, Gin, 1)
     print "\ngrupy.bands.out file written\n"
